1aa556102b8897f0fc61e5e273ef7c2ea9d2e34d
Maria Eduarda <mariaeduarda-souza@hotmail.com.br>
2024-09-10 16:17:16 -0300

    chore: fix implementation on main

--- ddd/src/main.ts
+++ ddd/src/main.ts
@@ -2,6 +2,7 @@ import Address from "./entity/address";
 import Customer from "./entity/customer";
 import Order from "./entity/order";
 import OrderItem from "./entity/orderItem";
+import Product from "./entity/product";
 
 // Id Relation
 let customer = new Customer("123", "Lorem Ipsum");
@@ -11,6 +12,8 @@ customer.address = address;
 customer.activate();
 
 // Entity relation
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 20);
+const product1 = new Product("1", "Product 1", 10);
+const product2 = new Product("2", "Product 2", 15);
+const item1 = new OrderItem("1", "1", "Item 1", 10, 2);
+const item2 = new OrderItem("2", "2", "Item 2", 20, 4);
 const order = new Order("1", "123", [item1, item2]);
