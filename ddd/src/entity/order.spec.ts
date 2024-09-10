dcb6de9db43d2cb6a95fc6633fda73eeb34be8e0
Maria Eduarda <mariaeduarda-souza@hotmail.com.br>
2024-09-10 16:01:20 -0300

    feat: product is now related to order item

--- ddd/src/entity/order.spec.ts
+++ ddd/src/entity/order.spec.ts
@@ -21,18 +21,25 @@ describe("Order unit test", () =>{
     });
 
     it("should calculate total", () => {
-        const item1 = new OrderItem("i1", "Item 1", 50);
-        const item2 = new OrderItem("i2", "Item 2", 40);
+        const item1 = new OrderItem("i1", "p1", "Item 1", 50, 4);
+        const item2 = new OrderItem("i2", "p2",  "Item 2", 40, 2);
         const order = new Order("o1", "c1", [item1]);
 
         let total = order.total();
 
-        expect(total).toBe(50);
+        expect(total).toBe(200);
 
         const order2 = new Order("o2", "c2", [item1, item2]);
         total = order2.total();
 
-        expect(total).toBe(90);
+        expect(total).toBe(280);
 
+    });
+
+    it("should throw error when item quantity is less or equal zero", () => {
+        expect(() => {
+            const item1 = new OrderItem("i1", "p1", "Item 1", 50, 0);
+            const order = new Order("12345", "12345", [item1]);
+        }).toThrow(new Error("Quantity must be greater than zero"));
     });
 });
