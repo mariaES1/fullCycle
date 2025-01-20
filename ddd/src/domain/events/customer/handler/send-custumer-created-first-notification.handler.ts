337c9f8faa11c9c60a4114a7a3d305e39db84b78
Maria Eduarda <maria.souza@contasimples.com>
2025-01-20 20:29:05 -0300

    feat: cria primeiro handler para notificação criação de cliente

---
+++ ddd/src/domain/events/customer/handler/send-custumer-created-first-notification.handler.ts
@@ -0,0 +1,10 @@
+import EventHandlerInterface from "../../@shared/interfaces/event-handler.interface";
+import CustomerCreatedEvent from "../customer-created.event";
+
+export default class CustomerCreatedFirstHandler
+  implements EventHandlerInterface<CustomerCreatedEvent>
+{
+  handle(): void {
+    console.log("Esse é o primeiro console.log do evento: CustomerCreated");
+  }
+}
